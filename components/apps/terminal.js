d994bcf309e9eb69e2e1f7016e09af80f49c4ae7
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-03-01 16:33:19 +0700

    main

--- components/apps/terminal.js
+++ components/apps/terminal.js
@@ -56,7 +56,7 @@ export class Terminal extends Component {
             <React.Fragment key={id}>
                 <div className="flex w-full h-5">
                     <div className="flex">
-                        <div className="text-ubt-green">nicholas@ASUS</div>
+                        <div className="text-ubt-green">bagus@UbuntuOS</div>
                         <div className="text-white mx-px font-medium">:</div>
                         <div className="text-ubt-blue">{this.current_directory}</div>
                         <div className="text-white mx-px font-medium mr-1">$</div>
