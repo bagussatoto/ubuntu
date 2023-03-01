5338e8264c69c2941168321dbdb3a3bb62f77304
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-03-01 17:16:57 +0700

    main

--- components/screen/lock_screen.js
+++ components/screen/lock_screen.js
@@ -23,7 +23,7 @@ export default function LockScreen(props) {
         <div id="ubuntu-lock-screen" style={{ zIndex: "100" }} className={(props.isLocked ? " visible translate-y-0 " : " invisible -translate-y-full ") + " absolute outline-none bg-black bg-opacity-90 transform duration-500 select-none top-0 right-0 overflow-hidden m-0 p-0 h-screen w-screen"}>
             <div style={{ backgroundImage: `url(${wallpapers[props.bgImgName]})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPositionX: "center" }} className="absolute top-0 left-0 w-full h-full transform z-20 blur-md "></div>
             <div className="w-full h-full z-50 overflow-hidden relative flex flex-col justify-center items-center text-white">
-                <img className="max-w-240 rounded-circle" src="./images/logos/white-round.jpg" alt="Nicholas Kato Logo" />
+                <img className="max-w-240 rounded-circle" src="./images/logos/bagus.jpg" alt="Bagus Logo" />
                 <br />
                 <br />
                 <div className="text-7xl">
